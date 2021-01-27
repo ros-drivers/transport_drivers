8dbc5b2eb80f910d7aae7608934b3f77dc10c551
flynneva <evanflynn.msu@gmail.com>
2021-01-27 13:51:54 -0800

    switched to component

--- udp_driver/include/udp_manager_node.hpp
+++ udp_driver/include/udp_manager_node.hpp
@@ -1,5 +1,19 @@
-#ifndef UDP_DRIVER__UDP_MANAGER_NODE_HPP_
-#define UDP_DRIVER__UDP_MANAGER_NODE_HPP_
+// Copyright 2021 Apex.AI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UDP_MANAGER_NODE_HPP_
+#define UDP_MANAGER_NODE_HPP_
 
 #include <rclcpp/rclcpp.hpp>
 
@@ -16,20 +30,19 @@ namespace udp_driver
 class UdpManagerNode : public rclcpp::Node
 {
   public:
-    UdpManagerNode();
-    ~UdpManagerNode(){}
+    UdpManagerNode(const rclcpp::NodeOptions & options);
 
-  private:
-    rclcpp::Service<udp_msgs::srv::UdpSocket>::SharedPtr create_udp_driver_;
-    std::map<uint16_t, std::shared_ptr<udp_msgs::srv::UdpSocket>> active_udp_drivers_;
+private:
+  rclcpp::Service<udp_msgs::srv::UdpSocket>::SharedPtr create_udp_driver_;
+  std::map<uint16_t, std::shared_ptr<udp_msgs::srv::UdpSocket>> active_udp_drivers_;
 
-    void create_driver_handler(
-      const std::shared_ptr<udp_msgs::srv::UdpSocket::Request> request,
-      const std::shared_ptr<udp_msgs::srv::UdpSocket::Response> response);
+  void create_driver_handler(
+    const std::shared_ptr<udp_msgs::srv::UdpSocket::Request> request,
+    const std::shared_ptr<udp_msgs::srv::UdpSocket::Response> response);
 };
 }  //  namespace udp_driver
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
 
@@ -42,4 +55,4 @@ int main(int argc, char **argv)
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Exiting...");
   return 0;
 }
-#endif  // UDP_DRIVER__UDP_MANAGER_NODE_HPP_
+#endif  // UDP_MANAGER_NODE_HPP_
