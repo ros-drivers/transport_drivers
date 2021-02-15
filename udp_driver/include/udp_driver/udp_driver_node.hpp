0a55a7127c70f23b5421d5b3096ace5a7a63172b
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-02-15 16:14:51 +0300

    Update Udp Driver Node

--- udp_driver/include/udp_driver/udp_driver_node.hpp
+++ udp_driver/include/udp_driver/udp_driver_node.hpp
@@ -23,11 +23,12 @@
 #include <chrono>
 #include <memory>
 #include <string>
+
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/qos.hpp"
 
-#include "udp_sender.h"
-#include "udp_receiver.h"
+#include "converters.hpp"
+#include "udp_driver.hpp"
 
 namespace autoware
 {
@@ -37,34 +38,9 @@ namespace drivers
 namespace udp_driver
 {
 
-struct Endpoint {
-  Endpoint(const std::string &ip, uint16_t port) : m_ip(ip), m_port(port) {}
-  const std::string &ip() const { return m_ip; }
-  uint16_t port() const { return m_port; }
-
-private:
-  std::string m_ip;
-  uint16_t m_port;
-};
-
-class UdpConfig {
-public:
-    UdpConfig(const Endpoint &client_endpoint, const Endpoint &binding_endpoint):
-            m_client_endpoint(client_endpoint),
-            m_binding_endpoint(binding_endpoint) {}
-
-    const Endpoint & client_endpoint() const { return m_client_endpoint; }
-    const Endpoint & binding_endpoint() const { return m_binding_endpoint; }
-
-private:
-    const Endpoint m_client_endpoint;
-    const Endpoint m_binding_endpoint;
-};
-
 /// \brief A node which encapsulates the primary functionality of a UDP receiver
 /// \tparam PacketT The type of the packet buffer. Typically a container
 /// \tparam OutputT The type a packet gets converted/deserialized into. Should be a ROS 2 message
-template<typename PacketT, typename OutputT>
 class UdpDriverNode : public rclcpp::Node
 {
 public:
@@ -77,107 +53,68 @@ public:
   UdpDriverNode(
     const std::string & node_name,
     const rclcpp::NodeOptions & options,
-    const UdpConfig & udp_config)
-  : Node(node_name, options),
-    m_pub_ptr(this->create_publisher<OutputT>("udp_read", rclcpp::QoS(10))),
-    m_sub_ptr(this->create_subscription<OutputT>("/udp_write",
-              rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(),
-              std::bind(&UdpDriverNode::sender_callback, this, std::placeholders::_1))),
-    m_sender(new UdpSender(udp_config.client_endpoint().ip(), udp_config.client_endpoint().port())),
-    m_receiver(new UdpReceiver(udp_config.binding_endpoint().ip(), udp_config.binding_endpoint().port())) {}
-
-  /// \brief Constructor - Gets config from ROS parameters
-  /// \param[in] node_name Name of node for rclcpp internals
-  /// \param[in] options rclcpp::NodeOptions instance containing options for the node
-  UdpDriverNode(
-    const std::string & node_name,
-    const rclcpp::NodeOptions & options)
-  : Node(node_name, options),
-    m_pub_ptr(Node::create_publisher<OutputT>("udp_read", rclcpp::QoS(10))),
-    m_sender(new UdpSender(declare_parameter("ip").get<std::string>(), static_cast<uint16_t>(declare_parameter("port").get<uint16_t>()))),
-    m_receiver(new UdpReceiver(declare_parameter("ip").get<std::string>(), static_cast<uint16_t>(declare_parameter("port").get<uint16_t>()))) {
+    IoContext &ctx) :
+        Node(node_name, options),
+        m_udp_driver(new UdpDriver(ctx)) {
   }
 
-  // brief Main loop: receives data from UDP, publishes to the given topic
-  void run(const uint32_t max_iterations = 0U)
-  {
-    // initialize the output object
-    OutputT output;
-    init_output(output);
+  ~UdpDriverNode() { std::cout << "[UdpDriverNode::~UdpDriverNode] INFO => Destructing..." << std::endl; }
 
-    uint32_t iter = 0U;
-    // workaround for rclcpp logging macros with template class
-    rclcpp::Logger node_logger = this->get_logger();
-
-    while (rclcpp::ok()) {
-      if ((max_iterations != 0U) && (max_iterations == iter)) {
-        break;
-      }
-      ++iter;
-      try {
-        PacketT pkt;
-        (void) m_receiver->receive_packet(pkt);
-
-        // message received, convert and publish
-        if (this->convertTo(pkt, output)) {
-          m_pub_ptr->publish(output);
-          while (this->get_output_remainder(output)) {
-            m_pub_ptr->publish(output);
-          }
+    void initialize_sender(const std::string &ip, int16_t port) {
+        m_udp_driver->initialize_sender(ip, port);
+        if (!m_udp_driver->sender()->isOpen()) {
+            m_udp_driver->sender()->open();
         }
-      } catch (const std::exception & e) {
-        RCLCPP_WARN(node_logger, e.what());
-        // And then just continue running
-      } catch (...) {
-        // Something really weird happened and I can't handle it here
-        RCLCPP_WARN(node_logger, "Unknown exception occured in UdpDriverNode");
-        throw;
-      }
+
+        createSubscribers();
     }
-  }
 
-protected:
-  /// \brief This method is called on the output just as the node execution thread is started.
-  ///        The main use case is preallocating unbounded messages (e.g. PointCloud2), or setting up
-  ///        headers.
-  /// \param[inout] output The message to get initialized
-  virtual void init_output(OutputT & output) = 0;
+    void initialize_receiver(const std::string &ip, uint16_t port) {
+        createPublishers();
 
-  /// \brief Converts a packet into an output, updates any stateful information
-  /// \param[in] pkt The packet type to be deserialized
-  /// \param[out] output Holds the output, if necessary
-  /// \return Whether or not an output has been written to (e.g. if sufficient points have been
-  ///         deserialized for a full PointCloud)
-  virtual bool convertTo(const PacketT & pkt, OutputT & output) = 0;
-
-  /// \brief Converts a ROS2 message into a packet buffer
-  /// \param[in] output Holds the output, if necessary
-  /// \param[out] pkt The packet type to be deserialized
-  /// \return Whether or not an output has been written to a packet buffer
-  virtual bool convertFrom(const OutputT & output, PacketT & pkt) = 0;
-
-  /// \brief Gets the remaining outputs from a packet. This supports the use case i.e. if a packet
-  ///        contains information for 500 points, but OutputT can only hold 300 points.
-  /// \param[out] output The extra output gets written into this variable
-  /// \return True if there are more outputs that must be published
-  ///         False if there are no more outputs to publish.
-  virtual bool get_output_remainder(OutputT & output) = 0;
+        m_udp_driver->initialize_receiver(ip, port);
+        m_udp_driver->receiver()->open();
+        m_udp_driver->receiver()->bind();
+        m_udp_driver->receiver()->asyncReceive(boost::bind(&UdpDriverNode::receiver_callback,this, _1));
+    }
 
 private:
-  void sender_callback(const typename OutputT::SharedPtr msg) {
-      PacketT pkt;
-      if (this->convertFrom(*msg, pkt)) {
-          if (m_sender->send(pkt) == -1) {
-              RCLCPP_WARN(this->get_logger(), "Cannot send Packet data on socket.");
-          }
-      }
+  std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>> publisher() const {
+      return m_publisher;
   }
 
-  const std::shared_ptr<typename rclcpp::Publisher<OutputT>> m_pub_ptr;
-  const std::shared_ptr<typename rclcpp::Subscription<OutputT>> m_sub_ptr;
+  void createPublishers() {
+    m_publisher = this->create_publisher<std_msgs::msg::Int32>("udp_read",
+                                                               rclcpp::QoS(100));
+  }
 
-  std::unique_ptr<UdpSender> m_sender;
-  std::unique_ptr<UdpReceiver> m_receiver;
+  void createSubscribers() {
+    m_subscriber = this->create_subscription<std_msgs::msg::Int32>("udp_write",
+        rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(),
+        std::bind(&UdpDriverNode::subscriber_callback, this, std::placeholders::_1));
+  }
+
+  void receiver_callback(const MutSocketBuffer &buffer) {
+      std::cout << "[UdpDriverNode::receiver_callback] " << *(int32_t*)buffer.data() << std::endl;
+
+      std_msgs::msg::Int32 out;
+      autoware::msgs::convertToRosMessage(buffer, out);
+
+      m_publisher->publish(out);
+  }
+
+  void subscriber_callback(std_msgs::msg::Int32::SharedPtr msg) {
+      std::cout << "[UdpDriverNode::subscriber_callback] " << msg->data << std::endl;
+
+      MutSocketBuffer out;
+      autoware::msgs::convertFromRosMessage(msg, out);
+
+      m_udp_driver->sender()->asyncSend(out);
+  }
+
+  std::shared_ptr<UdpDriver> m_udp_driver;
+  std::shared_ptr<typename rclcpp::Publisher<std_msgs::msg::Int32>> m_publisher;
+  std::shared_ptr<typename rclcpp::Subscription<std_msgs::msg::Int32>> m_subscriber;
 };  // class UdpDriverNode
 
 }  // namespace udp_driver
