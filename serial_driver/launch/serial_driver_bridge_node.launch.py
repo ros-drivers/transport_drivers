b0169e5d55db5f036ebb9b08f9bc526b5278e696
Joshua Whitley <josh.whitley@autoware.org>
2021-03-16 01:42:50 -0500

    Trying to apease flake8

--- serial_driver/launch/serial_driver_bridge_node.launch.py
+++ serial_driver/launch/serial_driver_bridge_node.launch.py
@@ -13,22 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_index_python.packages import get_package_share_directory
+import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import LifecycleNode
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch.actions import EmitEvent
 from launch.actions import RegisterEventHandler
+from launch.event_handlers.on_shutdown import OnShutdown
+from launch.events import matches_action
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events.lifecycle import ChangeState
 from launch_ros.events.lifecycle import matches_node_name
-from launch_ros.event_handlers import OnStateTransition
-from launch.events import matches_action
-from launch.event_handlers.on_shutdown import OnShutdown
-
 import lifecycle_msgs.msg
-import os
 
 
 def generate_launch_description():
